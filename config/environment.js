7f9a426088bcd6c49a4287d4e54b9da67c5be49b
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2017-08-14 10:03:57 +0200

    deps(leaflet/rollup): add dependencies

--- config/environment.js
+++ config/environment.js
@@ -6,5 +6,9 @@ module.exports = function(environment) {
     environment: environment
   };
 
+  if (environment === 'development') {
+    ENV.MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
+  }
+
   return ENV;
 };
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
-    // Add options here
+    rollup: {
+      plugins: [
+        resolve({ allowJS: true, jsnext: true, module: true, main: true }),
+        commonjs()
+      ]
+    }
   });
 
   return app.toTree();
